a829074dd968d514495ebdd4847f5e600bb81df8
Josias Mutshaila kasongo <josiaskasongo@MacBook-Pro-de-Josias.local>
2020-05-19 11:50:04 -0400

    preparation pour la mise en route

--- webapp/settings.py
+++ webapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^vh&45)vodwwnzg1p@l#v#pfmihw-n(^v5+@2f#bn*nha^6gyl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','178.128.238.24','josiaskasongo.com','www.josiaskasongo.com']
 
